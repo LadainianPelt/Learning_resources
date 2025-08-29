71629dc8d5d2081348893242dda34793fc9a2279
LadainianPelt <ljpelt5@gmail.com>
2025-08-29 15:52:12 -0500

    Add files via upload

---
+++ Lecture_2_finger_exercise.py
@@ -0,0 +1,7 @@
+#Assume you are given a variable named number (has a numerical value). Write a piece of Python code that prints out one of the following: positive if the variable number is positive, negative if the variable number is negative, zero if the variable number is equal to zero.
+if number >= 1
+print('positive')
+elif number <= -1
+print('negative')
+else:
+print('zero')

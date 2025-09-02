313a042ead3ec4d3ba9a7f6237e700cb0debafbf
LadainianPelt <ljpelt5@gmail.com>
2025-09-02 14:53:18 -0500

    Add files via upload

---
+++ Lecture_3_finger_exercise.py
@@ -0,0 +1,5 @@
+#Assume you are given a positive integer variable named N. Write a piece of Python code that prints hello world on separate, N times. You can use either a while loop or a for loop.
+n = 1
+for i in range(5):
+    print('hello world')
+    n += 1
